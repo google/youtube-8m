05ff8d0c0179add2b31d700ed05ca15be11b80c8
Nisarg Kothari <ndk@google.com>
2017-02-18 16:27:20 -0800

    Fix bug where the input queue was too small if you were using multiple GPUs.

--- train.py
+++ train.py
@@ -155,8 +155,8 @@ def get_input_data_tensors(reader,
     return tf.train.shuffle_batch_join(
         training_data,
         batch_size=batch_size,
-        capacity=FLAGS.batch_size * 5,
-        min_after_dequeue=FLAGS.batch_size,
+        capacity=batch_size * 5,
+        min_after_dequeue=batch_size,
         allow_smaller_final_batch=True,
         enqueue_many=True)
 
