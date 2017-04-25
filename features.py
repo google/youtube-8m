717029d9f8b30d7115441081b197cab5a75f3395
1o0ko <wojciech.stokowiec@gmail.com>
2017-04-25 20:55:04 +0000

    Added feature extraction

---
+++ features.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+import argparse
+import cv2
+import logging
+import numpy as np
+import os
+import pickle
+from PIL import Image
+import tensorflow as tf
+from tensorflow.python.platform import gfile
+import yaml
+
+os.environ['GLOG_minloglevel'] = '2'
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def load_file(file_name):
+    video_capture = cv2.VideoCapture(file_name)
+    return video_capture
+
+def get_scene_from_frames(video_capture, frames_directory_path):        
+    scenes = []
+    # Save one frame per interval
+    i = 0
+    while video_capture.isOpened():
+        position = 1000 * i
+        video_capture.set(cv2.CAP_PROP_POS_MSEC, position) # Go to the k sec. position
+        success, image = video_capture.read()
+        
+        if not success:
+            break
+        frame_path = os.path.join(
+                frames_directory_path, '%d.png') % i
+        scenes.append(frame_path)
+        cv2.imwrite(frame_path, image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+
+        i += 1
+        if i >= 360:
+            break
+
+    video_capture.release()
+    return scenes
+
+def create_graph(model_dir):
+    with gfile.FastGFile(model_dir, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+def compute_features(image, sess, next_to_last_tensor):
+    """
+    Extract features from image using inception v3 cnn.
+    Inputs:
+    -image: path to image
+    -sess: tensorflow session
+    -next_to_last_tensor: tensorflow tensor
+    Returns:
+    -features: list of extracted features
+    """
+    if not gfile.Exists(image):
+        tf.logging.fatal('File does not exist %s', image)
+    image_data = gfile.FastGFile(image, 'rb').read()
+    predictions = sess.run(next_to_last_tensor, {
+                           'DecodeJpeg/contents:0': image_data})
+    features = np.squeeze(predictions)
+    return features
+
+def extract_all_features(list_images_paths):
+    """
+    Extract features from list of images.
+    Inputs:
+    -image_list: list of image paths
+    Returns:
+    -img_features: list of extracted features
+    """
+    img_features = []
+
+    with tf.Session() as sess:
+
+        next_to_last_tensor = sess.graph.get_tensor_by_name('pool_3:0')
+
+        for image_path in list_images_paths:
+            features = compute_features(image_path, sess, next_to_last_tensor)
+            os.remove(image_path)
+            img_features.append(features)
+
+    return np.array(img_features, dtype = np.float32)
+        
+
+def process_scenes(scenes):
+    def convert_to_jpeg(list_img_filenames):
+        for img_path in list_img_filenames:
+            img_png = Image.open(img_path)
+            img_png.save(img_path, 'jpeg')  # convert to jpeg
+            
+    try: 
+        convert_to_jpeg(scenes)
+        if scenes:
+            img_features_dict = extract_all_features(scenes)
+    except IOError:
+            logging.info("No frame found in a video")
+            
+    return img_features_dict
+
+def dequantize(feat_vector, max_quantized_value=2, min_quantized_value=-2):
+    """Dequantize the feature from the byte format to the float format.
+    Args:
+        feat_vector: the input 1-d vector.
+        max_quantized_value: the maximum of the quantized value.
+        min_quantized_value: the minimum of the quantized value.
+    Returns:
+        A float vector which has the same shape as feat_vector.
+    """
+    assert max_quantized_value > min_quantized_value
+    quantized_range = max_quantized_value - min_quantized_value
+    scalar = quantized_range / 255.0
+    bias = (quantized_range / 512.0) + min_quantized_value
+    return feat_vector * scalar + bias
+
+def quantize(features, max_quantized_value=2, min_quantized_value=-2):
+    """Quantize the feature from the float format to the byte format.
+    Args:
+        features: the input 1-d vector.
+        max_quantized_value: the maximum of the quantized value.
+        min_quantized_value: the minimum of the quantized value.
+    Returns:
+        A float vector which has the same shape as feat_vector.
+    """
+    assert max_quantized_value > min_quantized_value
+    quantized_range = max_quantized_value - min_quantized_value
+    bias = (quantized_range / 512.0) + min_quantized_value
+    features = (features - bias) / quantized_range * 255
+    return np.uint8(features)
+    
+def video_scenes(video_name):
+    video_capture = load_file(video_name)
+    scenes = get_scene_from_frames(video_capture, '.')
+    return scenes
+
+def scenes_features(scenes, inception_model_path):
+    create_graph(inception_model_path)
+    features = process_scenes(scenes)
+    return features
+
+def decrease_dimension(features, pca_model_path):
+    # open PCA model
+    with open(pca_model_path, "rb") as file:
+        pca_obj = pickle.load(file, encoding='latin1')
+    
+    # reduce dimension and quantize data
+    features = pca_obj.transform(features)
+    features = np.array(features, dtype=np.float32)
+    q_features = quantize(features)
+    return q_features
+
+def video_features(video_name, inception_model_path, pca_model_path):
+    scenes = video_scenes(video_name)
+    features = scenes_features(scenes, inception_model_path)
+    
+    q_features = decrease_dimension(features, pca_model_path)
+    
+    # take average in columns
+    final_vector = np.mean(q_features, axis = 0)
+    final_vector = final_vector.reshape(1, len(final_vector))
+    return final_vector
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--video_path", help="Path to transformed video", required=True)
+    parser.add_argument("-m", "--model", help="Path inception model", required=True)
+    parser.add_argument("-p", "--pca", help="Path pca model", required=True)
+    args = parser.parse_args()
+    
+    video_name = args.video_path
+    inception_model_path = args.model
+    pca_model_path = args.pca
+    video_features_vector = video_features(video_name, inception_model_path, pca_model_path)
---
+++ inference_simple.py
@@ -0,0 +1,206 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Binary for generating predictions over a set of videos."""
+
+import os
+import time
+
+import numpy
+import tensorflow as tf
+
+from tensorflow import app
+from tensorflow import flags
+from tensorflow import gfile
+from tensorflow import logging
+
+import eval_util
+import losses
+import readers
+import utils
+
+FLAGS = flags.FLAGS
+
+if __name__ == '__main__':
+  flags.DEFINE_string("train_dir", "/tmp/yt8m_model/",
+                      "The directory to load the model files from.")
+  flags.DEFINE_string("output_file", "",
+                      "The file to save the predictions to.")
+  flags.DEFINE_string(
+      "input_data_pattern", "",
+      "File glob defining the evaluation dataset in tensorflow.SequenceExample "
+      "format. The SequenceExamples are expected to have an 'rgb' byte array "
+      "sequence feature as well as a 'labels' int64 context feature.")
+
+  # Model flags.
+  flags.DEFINE_bool(
+      "frame_features", False,
+      "If set, then --eval_data_pattern must be frame-level features. "
+      "Otherwise, --eval_data_pattern must be aggregated video-level "
+      "features. The model must also be set appropriately (i.e. to read 3D "
+      "batches VS 4D batches.")
+  flags.DEFINE_integer(
+      "batch_size", 4096,
+      "How many examples to process per batch.")
+  flags.DEFINE_string("feature_names", "mean_rgb", "Name of the feature "
+                      "to use for training.")
+  flags.DEFINE_string("feature_sizes", "1024", "Length of the feature vectors.")
+
+
+  # Other flags.
+  flags.DEFINE_integer("num_readers", 1,
+                       "How many threads to use for reading input files.")
+  flags.DEFINE_integer("top_k", 20,
+                       "How many predictions to output per video.")
+
+def format_lines(video_ids, predictions, top_k):
+  batch_size = len(video_ids)
+  for video_index in range(batch_size):
+    top_indices = numpy.argpartition(predictions[video_index], -top_k)[-top_k:]
+    line = [(class_index, predictions[video_index][class_index])
+            for class_index in top_indices]
+    line = sorted(line, key=lambda p: -p[1])
+    yield video_ids[video_index].decode('utf-8') + "," + " ".join("%i %f" % pair
+                                                  for pair in line) + "\n"
+
+
+def get_input_data_tensors(reader, data_pattern, batch_size, num_readers=1):
+  """Creates the section of the graph which reads the input data.
+
+  Args:
+    reader: A class which parses the input data.
+    data_pattern: A 'glob' style path to the data files.
+    batch_size: How many examples to process at a time.
+    num_readers: How many I/O threads to use.
+
+  Returns:
+    A tuple containing the features tensor, labels tensor, and optionally a
+    tensor containing the number of frames per video. The exact dimensions
+    depend on the reader being used.
+
+  Raises:
+    IOError: If no files matching the given pattern were found.
+  """
+  with tf.name_scope("input"):
+    files = gfile.Glob(data_pattern)
+    if not files:
+      raise IOError("Unable to find input files. data_pattern='" +
+                    data_pattern + "'")
+    logging.info("number of input files: " + str(len(files)))
+
+    ilename_queue = tf.train.string_input_producer(
+        files, num_epochs=1, shuffle=False)
+
+    examples_and_labels = [reader.prepare_reader(filename_queue)
+                           for _ in range(num_readers)]
+
+    video_id_batch, video_batch, unused_labels, num_frames_batch = (
+        tf.train.batch_join(examples_and_labels,
+                            batch_size=batch_size,
+                            allow_smaller_final_batch=True,
+                            enqueue_many=True))
+    return video_id_batch, video_batch, num_frames_batch
+
+def inference(reader, train_dir, data_pattern, out_file_location, batch_size, top_k):
+  with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess, gfile.Open(out_file_location, "w+") as out_file:
+    video_id_batch, video_batch, num_frames_batch = get_input_data_tensors(reader, data_pattern, batch_size)
+    latest_checkpoint = tf.train.latest_checkpoint(train_dir)
+    if latest_checkpoint is None:
+      raise Exception("unable to find a checkpoint at location: %s" % train_dir)
+    else:
+      meta_graph_location = latest_checkpoint + ".meta"
+      logging.info("loading meta-graph: " + meta_graph_location)
+      logging.info("loaded graph!")
+
+    saver = tf.train.import_meta_graph(meta_graph_location, clear_devices=True)
+    logging.info("restoring variables from " + latest_checkpoint)
+    saver.restore(sess, latest_checkpoint)
+    logging.info("Restored variables!")
+
+    input_tensor = tf.get_collection("input_batch_raw")[0]
+    num_frames_tensor = tf.get_collection("num_frames")[0]
+    predictions_tensor = tf.get_collection("predictions")[0]
+
+    # Workaround for num_epochs issue.
+    def set_up_init_ops(variables):
+      init_op_list = []
+      for variable in list(variables):
+        if "train_input" in variable.name:
+          init_op_list.append(tf.assign(variable, 1))
+          variables.remove(variable)
+      init_op_list.append(tf.variables_initializer(variables))
+      return init_op_list
+
+    sess.run(set_up_init_ops(tf.get_collection_ref(
+        tf.GraphKeys.LOCAL_VARIABLES)))
+
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    num_examples_processed = 0
+    start_time = time.time()
+    out_file.write("VideoId,LabelConfidencePairs\n")
+
+    try:
+      while not coord.should_stop():
+          video_id_batch_val, video_batch_val,num_frames_batch_val = sess.run([video_id_batch, video_batch, num_frames_batch])
+          predictions_val, = sess.run([predictions_tensor], feed_dict={
+              input_tensor: video_batch_val,
+              num_frames_tensor: num_frames_batch_val
+          })
+          now = time.time()
+          num_examples_processed += len(video_batch_val)
+          num_classes = predictions_val.shape[1]
+          logging.info("num examples processed: " + str(num_examples_processed) + " elapsed seconds: " + "{0:.2f}".format(now-start_time))
+          for line in format_lines(video_id_batch_val, predictions_val, top_k):
+            out_file.write(line)
+          out_file.flush()
+
+
+    except tf.errors.OutOfRangeError:
+        logging.info('Done with inference. The output file was written to ' + out_file_location)
+    finally:
+        coord.request_stop()
+
+    coord.join(threads)
+    sess.close()
+
+
+def main(unused_argv):
+  logging.set_verbosity(tf.logging.INFO)
+
+  # convert feature_names and feature_sizes to lists of values
+  feature_names, feature_sizes = utils.GetListOfFeatureNamesAndSizes(
+      FLAGS.feature_names, FLAGS.feature_sizes)
+
+  if FLAGS.frame_features:
+    reader = readers.YT8MFrameFeatureReader(feature_names=feature_names,
+                                            feature_sizes=feature_sizes)
+  else:
+    reader = readers.YT8MAggregatedFeatureReader(feature_names=feature_names,
+                                                 feature_sizes=feature_sizes)
+
+  if FLAGS.output_file is "":
+    raise ValueError("'output_file' was not specified. "
+      "Unable to continue with inference.")
+
+  if FLAGS.input_data_pattern is "":
+    raise ValueError("'input_data_pattern' was not specified. "
+      "Unable to continue with inference.")
+
+  inference(reader, FLAGS.train_dir, FLAGS.input_data_pattern,
+    FLAGS.output_file, FLAGS.batch_size, FLAGS.top_k)
+
+
+if __name__ == "__main__":
+  app.run()
