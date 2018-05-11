2200901800076ebec7530d052d966e4b178b7daa
Sami Abu-El-Haija <haija@google.com>
2018-05-11 14:34:39 -0700

    Inference on CPU

--- inference.py
+++ inference.py
@@ -137,8 +137,8 @@ def inference(reader, train_dir, data_pattern, out_file_location, batch_size, to
         tar.add(os.path.join(FLAGS.train_dir, "model_flags.json"),
                 arcname="model_flags.json")
       print('Tarred model onto ' + FLAGS.output_model_tgz)
-
-    saver = tf.train.import_meta_graph(meta_graph_location, clear_devices=True)
+    with tf.device("/cpu:0"):
+      saver = tf.train.import_meta_graph(meta_graph_location, clear_devices=True)
     logging.info("restoring variables from " + checkpoint_file)
     saver.restore(sess, checkpoint_file)
     input_tensor = tf.get_collection("input_batch_raw")[0]
