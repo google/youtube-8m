b10751ec23eede327e4663002968238d4a61a706
Zheng Xu <zhengxu@google.com>
2019-08-21 13:51:39 -0700

    Yapf fix for model_utils.py.

--- model_utils.py
+++ model_utils.py
@@ -32,17 +32,16 @@ def SampleRandomSequence(model_input, num_frames, num_samples):
   """
 
   batch_size = tf.shape(model_input)[0]
-  frame_index_offset = tf.tile(
-      tf.expand_dims(tf.range(num_samples), 0), [batch_size, 1])
+  frame_index_offset = tf.tile(tf.expand_dims(tf.range(num_samples), 0),
+                               [batch_size, 1])
   max_start_frame_index = tf.maximum(num_frames - num_samples, 0)
   start_frame_index = tf.cast(
-      tf.multiply(
-          tf.random_uniform([batch_size, 1]),
-          tf.cast(max_start_frame_index + 1, tf.float32)), tf.int32)
+      tf.multiply(tf.random_uniform([batch_size, 1]),
+                  tf.cast(max_start_frame_index + 1, tf.float32)), tf.int32)
   frame_index = tf.minimum(start_frame_index + frame_index_offset,
                            tf.cast(num_frames - 1, tf.int32))
-  batch_index = tf.tile(
-      tf.expand_dims(tf.range(batch_size), 1), [1, num_samples])
+  batch_index = tf.tile(tf.expand_dims(tf.range(batch_size), 1),
+                        [1, num_samples])
   index = tf.stack([batch_index, frame_index], 2)
   return tf.gather_nd(model_input, index)
 
@@ -60,11 +59,11 @@ def SampleRandomFrames(model_input, num_frames, num_samples):
   """
   batch_size = tf.shape(model_input)[0]
   frame_index = tf.cast(
-      tf.multiply(
-          tf.random_uniform([batch_size, num_samples]),
-          tf.tile(tf.cast(num_frames, tf.float32), [1, num_samples])), tf.int32)
-  batch_index = tf.tile(
-      tf.expand_dims(tf.range(batch_size), 1), [1, num_samples])
+      tf.multiply(tf.random_uniform([batch_size, num_samples]),
+                  tf.tile(tf.cast(num_frames, tf.float32), [1, num_samples])),
+      tf.int32)
+  batch_index = tf.tile(tf.expand_dims(tf.range(batch_size), 1),
+                        [1, num_samples])
   index = tf.stack([batch_index, frame_index], 2)
   return tf.gather_nd(model_input, index)
 
